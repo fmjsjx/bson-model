c76b234b6aaadaeb7c6453010a93ba6a6e7ed470
fmjsjx <fmjsjx@163.com>
2021-11-09 15:26:17 +0800

    Upgrade dependencies
    libcommon  2.5.0    -> 2.5.2
    jackson    2.12.4   -> 2.13.0
    logabck    1.2.5    -> 1.2.6
    jruby      9.2.19.0 -> 9.3.1.0

--- buildSrc/src/main/kotlin/bson-model.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/bson-model.java-library-conventions.gradle.kts
@@ -13,26 +13,26 @@ dependencies {
     // netty-bom
     api(platform("io.netty:netty-bom:4.1.69.Final"))
     // libcommon-bom
-    api(platform("com.github.fmjsjx:libcommon-bom:2.5.0"))
+    api(platform("com.github.fmjsjx:libcommon-bom:2.5.2"))
     // junit-bom
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.12.4"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.13.0"))
 
     constraints {
         implementation("org.slf4j:slf4j-api:1.7.32")
-        implementation("ch.qos.logback:logback-classic:1.2.5")
-        api("com.dslplatform:dsl-json-java8:1.9.8")
+        implementation("ch.qos.logback:logback-classic:1.2.6")
+        api("com.dslplatform:dsl-json-java8:1.9.9")
         api("com.jsoniter:jsoniter:0.9.23")
         api("org.mongodb:bson:4.3.4")
         api("org.mongodb:mongodb-driver-core:4.3.4")
         api("org.mongodb:mongodb-driver-sync:4.3.4")
         api("org.mongodb:mongodb-driver-reactivestreams:4.3.4")
         api("org.mongodb:mongodb-driver-legacy:4.3.4")
-        implementation("org.jruby:jruby-complete:9.2.19.0")
-        implementation("org.jruby:jruby:9.2.19.0")
-        implementation("org.jruby:jruby-core:9.2.19.0")
-        implementation("org.jruby:jruby-stdlib:9.2.19.0")
+        implementation("org.jruby:jruby-complete:9.3.1.0")
+        implementation("org.jruby:jruby:9.3.1.0")
+        implementation("org.jruby:jruby-core:9.3.1.0")
+        implementation("org.jruby:jruby-stdlib:9.3.1.0")
         implementation("org.yaml:snakeyaml:1.29")
     }
     // log4j2
