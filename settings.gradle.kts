e4cd7001bd2120ff47f3bc2a16ece59528e79572
fmjsjx <fmjsjx@163.com>
2022-01-24 12:59:35 +0800

    Set gradle source to China Mirrors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+pluginManagement {
+    repositories {
+        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+    }
+}
+
 rootProject.name = "bson-model"
 include(":bson-model-bom")
 include(":bson-model-core")
