5cb34ad3b7932fefdbe80966c16bfa0e1bdb8521
fmjsjx <fmjsjx@163.com>
2024-10-11 14:28:09 +0800

    Add more plugin mirrors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,13 @@
 pluginManagement {
     repositories {
-        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven {
+            url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
         gradlePluginPortal()
+        mavenCentral()
     }
 }
 
